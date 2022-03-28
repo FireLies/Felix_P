0273a109d72bb457550f272d2b7f29b0cc151de2
FireLies <jeantitor@protonmail.com>
2022-03-28 20:31:04 +0700

    Added some 'features'

--- Felix.py
+++ Felix.py
@@ -1,8 +1,13 @@
+import time
 import os
 import sys
 import glob
 import argparse
 
+
+print("""
+Hello Felix_P! -| v1.1 |- FireLies 2022 -> https://github.com/FireLies/Felix_P
+\nUsage: py felix.py [--type <list>] [(opt) --path <str>]""")
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -21,35 +26,38 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-raw_Path = str(*args.path)
+# Checking user input
+#--------------------------------------------------------------
+RawPath = str(*args.path)
+args.type = list(dict.fromkeys(args.type))
 with open('.\\Misc\\Valid_Extensions.txt', 'r') as valid:
     ValidType = valid.read().split("\n")
 
-# Checking user input
-#--------------------------------------------------------------
-if os.path.exists(raw_Path) == False:
-    print("\n--Cannot find path:", raw_Path, "\n")
+if os.path.exists(RawPath) == False:
+    print("\n--Cannot find path:", RawPath, "\n")
     sys.exit(0)
 
 if not args.type:
-    print("\n[--type] argumment cannot be empty.\n")
+    print("\n[--type] argumment required, at least 1 value. Usage: py felix.py --type <list>\n")
     sys.exit(0)
 
-check_Type = [_ for _ in args.type if _ not in ValidType]
-if check_Type:
-    print("\n--Invalid file type:", ', '.join(check_Type))
-    for i in check_Type:
-        args.type.remove(i)
-    print("\nValid file type:", ''.join(ValidType))
+CheckType = [i for i in args.type if i not in ValidType]
+if CheckType:
+    args.type = [i for i in args.type if i not in CheckType]
+    print("\n--Invalid file type:", ', '.join(CheckType), "\n\nValid file type:", ', '.join(ValidType))
+    if len(args.type) == 0:
+        print()
+        sys.exit(0)
 #--------------------------------------------------------------
 
+stw_Start = time.time()
 removed, failed = 0, 0
-for ext in args.type:
-    print(f"\n--Trying: {ext} in {raw_Path}\...")
-    check_File = glob.glob(raw_Path + "\\**\\*" + ext, recursive=True)
 
+for ext in args.type:
+    print(f"\n--Trying: {ext} in {RawPath}\...")
+    check_File = glob.glob(RawPath + "\\**\\*" + ext, recursive=True)
     for files in check_File:
-        with open(files, "w") as content:
+        with open(files, 'w') as content:
             content.write("")
 
         if os.path.getsize(files) > 0:
@@ -61,4 +69,5 @@ for ext in args.type:
     if len(check_File) == 0:
         print(f"--Cannot find {ext} file(s)")
 
-print(f"\nSuccess: {removed}  //  Failed: {failed}\n")
+stw_Result = str(round((time.time() - stw_Start) * 1000, 3))
+print(f"\n--Removed in {stw_Result} ms. Total: {removed + failed} | Success: {removed} | Failed: {failed}\n")
